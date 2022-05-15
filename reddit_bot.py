74eeab5f928d6d5cddff7cb00174155e31179dee
benyoon2003 <102326657+benyoon2003@users.noreply.github.com>
2022-05-15 16:51:21 -0700

    Add files via upload

--- reddit_bot.py
+++ reddit_bot.py
@@ -4,39 +4,6 @@ from heapq import nlargest
 import tkinter as tk
 from tkinter import *
 
-class GUI:
-    def __int__(self):
-        self.selection = selection
-
-    def interface(self):
-        r = tk.Tk()
-        r.geometry('500x500')
-
-        menubutton = Menubutton(r, text="File", width=35)
-        menubutton.grid()
-        menubutton.menu = Menu(menubutton)
-        menubutton["menu"] = menubutton.menu
-        menubutton.menu.add_checkbutton(label="New file", variable=IntVar())
-        menubutton.menu.add_checkbutton(label="Save", variable=IntVar())
-        menubutton.menu.add_checkbutton(label="Save as", variable=IntVar())
-        menubutton.pack()
-
-        instructions = Label(r, text="Select one of the following subreddits to scrape")
-        wallst_button = tk.Button(r, text='r/wallstreetbets', width=15, command= lambda: p.scrape_and_rank('wallstreetbets'))
-        stocks_button = tk.Button(r, text='r/stocks', width=15, command= lambda: p.scrape_and_rank('stocks'))
-        stock_market_button = tk.Button(r, text='r/StockMarket', width=15, command= lambda: p.scrape_and_rank('StockMarket'))
-        stock_picks_button = tk.Button(r, text='r/Stock_Picks', width=15, command= lambda: p.scrape_and_rank('Stock_Picks'))
-
-        stocks = Label(r, text=f"The top three most discussed stocks currently are {p.first}, {p.second}, {p.third}.")
-
-        instructions.pack()
-        wallst_button.pack()
-        stocks_button.pack()
-        stock_market_button.pack()
-        stock_picks_button.pack()
-        stocks.pack()
-        r.mainloop()
-
 
 class back_end:
     def __init__(self):
@@ -44,6 +11,15 @@ class back_end:
         self.first = None
         self.second = None
         self.third = None
+
+    def get_first_stock(self):
+        return self.first
+
+    def get_second_stock(self):
+        return self.second
+
+    def get_third_stock(self):
+        return self.third
 
     def create_stock_list(self):
         with open('nasdaq') as f:
@@ -81,6 +57,42 @@ class back_end:
         print(res)
 
 
+class GUI:
+    def __init__(self):
+        self.stocks = None
+
+    def update_text(self):
+        self.stocks.config(text= f"The top three most discussed stocks currently are {p.get_first_stock()}, {p.get_second_stock()}, {p.get_third_stock()}. \n "
+                               f"Here is some info about these stocks:")
+
+    def interface(self):
+        r = tk.Tk()
+        r.geometry('500x500')
+
+        menubutton = Menubutton(r, text="File", width=35)
+        menubutton.grid()
+        menubutton.menu = Menu(menubutton)
+        menubutton["menu"] = menubutton.menu
+        menubutton.menu.add_checkbutton(label="New file", variable=IntVar())
+        menubutton.menu.add_checkbutton(label="Save", variable=IntVar())
+        menubutton.menu.add_checkbutton(label="Save as", variable=IntVar())
+        menubutton.pack()
+
+        instructions = Label(r, text="Select one of the following subreddits to scrape")
+        wallst_button = tk.Button(r, text='r/wallstreetbets', width=15, command= lambda: [p.scrape_and_rank('wallstreetbets'), x.update_text()])
+        stocks_button = tk.Button(r, text='r/stocks', width=15, command= lambda: [p.scrape_and_rank('stocks'), x.update_text()])
+        stock_market_button = tk.Button(r, text='r/StockMarket', width=15, command= lambda: [p.scrape_and_rank('StockMarket'), x.update_text()])
+        stock_picks_button = tk.Button(r, text='r/Stock_Picks', width=15, command= lambda: [p.scrape_and_rank('Stock_Picks'), x.update_text()])
+        self.stocks = Label(r, text="")
+
+        instructions.pack()
+        wallst_button.pack()
+        stocks_button.pack()
+        stock_market_button.pack()
+        stock_picks_button.pack()
+        self.stocks.pack()
+        r.mainloop()
+
 
 
 p = back_end()
@@ -90,6 +102,7 @@ x = GUI()
 x.interface()
 
 
-#so far program return three most mentioned stocks mentioned in wallstreet bets by stock abbreviation
-#must turn everything into classes, create GUI, and return links and info about stock
+
+#GUI does not update after three stocks are loaded
+#must return links and info about stock
 
