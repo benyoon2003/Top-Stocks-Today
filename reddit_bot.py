7b5b83cd072bc25dc3aca2ec20ac14fe858b4abb
benyoon2003 <102326657+benyoon2003@users.noreply.github.com>
2022-05-16 17:12:46 -0400

    Add files via upload

--- reddit_bot.py
+++ reddit_bot.py
@@ -3,6 +3,7 @@ import praw
 from heapq import nlargest
 import tkinter as tk
 from tkinter import *
+import yfinance as yahooFinance
 
 
 class back_end:
@@ -11,6 +12,9 @@ class back_end:
         self.first = None
         self.second = None
         self.third = None
+        self.first_stock_info = None
+        self.second_stock_info = None
+        self.third_stock_info = None
 
     def get_first_stock(self):
         return self.first
@@ -20,6 +24,15 @@ class back_end:
 
     def get_third_stock(self):
         return self.third
+
+    def get_first_stock_info(self):
+        return self.first_stock_info
+
+    def get_second_stock_info(self):
+        return self.second_stock_info
+
+    def get_third_stock_info(self):
+        return self.third_stock_info
 
     def create_stock_list(self):
         with open('nasdaq') as f:
@@ -51,30 +64,73 @@ class back_end:
         self.first = res[0]
         self.second = res[1]
         self.third = res[2]
-
-        print(res)
+        self.first_stock_info = yahooFinance.Ticker(self.first)
+        self.second_stock_info = yahooFinance.Ticker(self.second)
+        self.third_stock_info = yahooFinance.Ticker(self.third)
 
 
 class GUI:
     def __init__(self):
         self.stocks = None
+        self.stock1_info = None
+        self.stock2_info = None
+        self.stock3_info = None
+        self.line1 = None
+        self.line2 = None
+        self.line3 = None
+        self.line4 = None
 
     def update_text(self):
         self.stocks.config(text= f"The top three most discussed stocks currently are {p.get_first_stock()}, {p.get_second_stock()}, {p.get_third_stock()}. \n "
                                f"Here is some info about these stocks:")
+        self.stock1_info.config(text=f"{p.get_first_stock()}/{p.get_first_stock_info().info['longName']}")
+        self.line1.config(text=f"Price: {p.get_first_stock_info().info['currentPrice']}\n"
+                               f"Open: {p.get_first_stock_info().info['open']}  High: {p.get_first_stock_info().info['dayHigh']}    "
+                               f"Low: {p.get_first_stock_info().info['dayLow']}\n"
+                               f"Mkt Cap: {p.get_first_stock_info().info['marketCap']}  Forward P/E: "
+                               f"{p.get_first_stock_info().info['forwardPE']}  "
+                               f"Div&Yield: {p.get_first_stock_info().info['dividendYield']}\n"
+                               f"Prev. Close: {p.get_first_stock_info().info['previousClose']}  52 Wk. Low: "
+                               f"{p.get_first_stock_info().info['fiftyTwoWeekLow']}  52 Wk. High: "
+                               f"{p.get_first_stock_info().info['fiftyTwoWeekHigh']}\n \n"
+                               f"{p.get_first_stock_info().info['longBusinessSummary']}\n\n\n", wraplength=600)
+        self.stock2_info.config(text=f"{p.get_second_stock()}/{p.get_second_stock_info().info['longName']}")
+        self.line2.config(text=f"Price: {p.get_second_stock_info().info['currentPrice']}\n"
+                               f"Open: {p.get_second_stock_info().info['open']}  High: {p.get_second_stock_info().info['dayHigh']}    "
+                               f"Low: {p.get_second_stock_info().info['dayLow']}\n"
+                               f"Mkt Cap: {p.get_second_stock_info().info['marketCap']}  Forward P/E: "
+                               f"{p.get_second_stock_info().info['forwardPE']}  "
+                               f"Div&Yield: {p.get_second_stock_info().info['dividendYield']}\n"
+                               f"Prev. Close: {p.get_second_stock_info().info['previousClose']}  52 Wk. Low: "
+                               f"{p.get_second_stock_info().info['fiftyTwoWeekLow']}  52 Wk. High: "
+                               f"{p.get_second_stock_info().info['fiftyTwoWeekHigh']}\n \n"
+                               f"{p.get_second_stock_info().info['longBusinessSummary']}", wraplength=600)
+        self.stock3_info.config(text=f"{p.get_third_stock()}/{p.get_third_stock_info().info['longName']}")
+        self.line3.config(text=f"Price: {p.get_third_stock_info().info['currentPrice']}\n"
+                               f"Open: {p.get_third_stock_info().info['open']}  High: {p.get_third_stock_info().info['dayHigh']}    "
+                               f"Low: {p.get_third_stock_info().info['dayLow']}\n"
+                               f"Mkt Cap: {p.get_third_stock_info().info['marketCap']}  Forward P/E: "
+                               f"{p.get_third_stock_info().info['forwardPE']}  "
+                               f"Div&Yield: {p.get_third_stock_info().info['dividendYield']}\n"
+                               f"Prev. Close: {p.get_third_stock_info().info['previousClose']}  52 Wk. Low: "
+                               f"{p.get_third_stock_info().info['fiftyTwoWeekLow']}  52 Wk. High: "
+                               f"{p.get_third_stock_info().info['fiftyTwoWeekHigh']}\n \n"
+                               f"{p.get_third_stock_info().info['longBusinessSummary']}", wraplength=250)
+
 
     def interface(self):
         r = tk.Tk()
-        r.geometry('500x500')
+        r.geometry('640x800')
 
-        menubutton = Menubutton(r, text="File", width=35)
-        menubutton.grid()
-        menubutton.menu = Menu(menubutton)
-        menubutton["menu"] = menubutton.menu
-        menubutton.menu.add_checkbutton(label="New file", variable=IntVar())
-        menubutton.menu.add_checkbutton(label="Save", variable=IntVar())
-        menubutton.menu.add_checkbutton(label="Save as", variable=IntVar())
-        menubutton.pack()
+
+        # menubutton = Menubutton(r, text="File", width=35)
+        # menubutton.grid()
+        # menubutton.menu = Menu(menubutton)
+        # menubutton["menu"] = menubutton.menu
+        # menubutton.menu.add_checkbutton(label="New file", variable=IntVar())
+        # menubutton.menu.add_checkbutton(label="Save", variable=IntVar())
+        # menubutton.menu.add_checkbutton(label="Save as", variable=IntVar())
+        # menubutton.pack()
 
         instructions = Label(r, text="Select one of the following subreddits to scrape")
         wallst_button = tk.Button(r, text='r/wallstreetbets', width=15, command= lambda: [p.scrape_and_rank('wallstreetbets'), x.update_text()])
@@ -82,6 +138,14 @@ class GUI:
         stock_market_button = tk.Button(r, text='r/StockMarket', width=15, command= lambda: [p.scrape_and_rank('StockMarket'), x.update_text()])
         stock_picks_button = tk.Button(r, text='r/Stock_Picks', width=15, command= lambda: [p.scrape_and_rank('Stock_Picks'), x.update_text()])
         self.stocks = Label(r, text="")
+        space = Label(r, text= "")
+        self.stock1_info = Label(r, text="")
+        self.stock2_info = Label(r, text="")
+        self.stock3_info = Label(r, text="")
+        self.line1 = Label(r, text="")
+        self.line2 = Label(r, text="")
+        self.line3 = Label(r, text="")
+        self.line4 = Label(r, text="")
 
         instructions.pack()
         wallst_button.pack()
@@ -89,6 +153,16 @@ class GUI:
         stock_market_button.pack()
         stock_picks_button.pack()
         self.stocks.pack()
+        space.pack()
+        self.stock1_info.pack()
+        self.line1.pack()
+        self.stock2_info.pack()
+        self.line2.pack()
+        self.stock3_info.pack()
+        self.line3.pack()
+        self.line4.pack()
+
+
         r.mainloop()
 
 
@@ -106,4 +180,3 @@ x.interface()
 #current event articles links- these are the top 3 articles that pop up
 
 #https://thecleverprogrammer.com/2020/08/22/real-time-stock-price-with-python/
-
